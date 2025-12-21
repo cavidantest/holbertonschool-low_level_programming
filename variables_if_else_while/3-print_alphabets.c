e3d9796e82a327e9d24625774fdbfe0d6603473c
cavidantest <contact.cavidan.sultan@gmail.com>
2025-12-21 17:17:12 +0000

    3 task

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, then uppercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
