8a6b7131507b3ea7c8fda679a6bec26ba5a973a3
cavidantest <contact.cavidan.sultan@gmail.com>
2025-12-22 05:54:08 +0000

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ */
+void print_to_98(int n)
+{
+	int step;
+
+	if (n == 98)
+	{
+		printf("98\n");
+		return;
+	}
+
+	step = (n < 98) ? 1 : -1;
+
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		n += step;
+	}
+	printf("98\n");
+}
