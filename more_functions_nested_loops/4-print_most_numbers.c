1e415b5f3e04920d4e448d4a55d6d3951cd8a7c6
cavidantest <contact.cavidan.sultan@gmail.com>
2025-12-25 17:25:53 +0000

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+			_putchar('0' + n);
+		n++;
+	}
+	_putchar('\n');
+}
