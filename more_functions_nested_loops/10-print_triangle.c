b0d3dcfdc704a48b4effbe1a21ca00e4d2d5c40d
cavidantest <contact.cavidan.sultan@gmail.com>
2025-12-25 17:36:08 +0000

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int row, spaces, hashes;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (spaces = size - row; spaces > 0; spaces--)
+			_putchar(' ');
+
+		for (hashes = 0; hashes < row; hashes++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
