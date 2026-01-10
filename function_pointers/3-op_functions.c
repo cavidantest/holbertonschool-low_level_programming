a36e105b428b609b5124afe778b6e820de655f62
cavidantest <contact.cavidan.sultan@gmail.com>
2026-01-10 05:07:00 +0000

    task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns sum
+ * @a: int
+ * @b: int
+ * Return: sum
+ */
+int op_add(int a, int b) { return (a + b); }
+
+/**
+ * op_sub - returns difference
+ * @a: int
+ * @b: int
+ * Return: difference
+ */
+int op_sub(int a, int b) { return (a - b); }
+
+/**
+ * op_mul - returns product
+ * @a: int
+ * @b: int
+ * Return: product
+ */
+int op_mul(int a, int b) { return (a * b); }
+
+/**
+ * op_div - returns division
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_div(int a, int b) { return (a / b); }
+
+/**
+ * op_mod - returns remainder
+ * @a: int
+ * @b: int
+ * Return: remainder
+ */
+int op_mod(int a, int b) { return (a % b); }
